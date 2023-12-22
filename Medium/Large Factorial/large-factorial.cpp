e3cec3c1bd99c6dfb38cf52ef1f4039de72f2d22
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-12-22 21:09:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Large Factorial/large-factorial.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+// int m = 1000000007;
+
+// class Solution{
+// public:
+  
+//     long long fact(int num){
+//         if(num==0 || num==1){
+//             return 1;
+//         }
+//       long long   res =1;
+//         for(int i=2;i<=num;i++){
+//             res = (res * i )%m;
+//         }
+//       return res;
+//     }
+    
+//      vector<long long> factorial(vector<long long> a, int n) {
+//          vector<long long >v;
+         
+//          for(int i=0;i<n;i++){
+//              long long number= fact(a[i]);
+//              v.push_back(number);
+             
+//          }
+//          return v;
+        
+//     }
+// };
+
+
+
+class Solution{
+public:
+    vector<long long> factorial(vector<long long> a, int n) {
+        int i, MAX = 1e5;
+        long long fac[MAX + 1], mod = 1e9 + 7;
+        fac[0] = 1;
+        for (i = 1; i <= MAX; i++) {
+            fac[i] = (fac[i - 1] * (long long)i) % mod;
+        }
+        
+        vector<long long> ans(n);
+        for (i = 0; i < n; i++) {
+            ans[i] = fac[a[i]];
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<long long> a(n);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        vector<long long> res = ob.factorial(a, n);
+        for (i = 0; i < n; i++) {
+            cout << res[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
