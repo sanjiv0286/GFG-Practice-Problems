961e91663132e5df2237341322850480cc4fb50e
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-12-23 09:04:16 +0530

    Updated Solution - GfG to GitHub

--- Medium/Factorials of large numbers/factorials-of-large-numbers.cpp
+++ Medium/Factorials of large numbers/factorials-of-large-numbers.cpp
@@ -8,39 +8,79 @@ using namespace std;
 
 class Solution {
 public:
-
-    void  multiply(vector<int>&arr,int &size,int mul){
+    void multiply( vector<int>&v,int &si,int mul ){
         int carry =0;
-        for(int i=0;i<size;i++){
-            int res= mul*arr[i];
-            res = res+carry;
-            arr[i]=res%10;
-            carry = res/10;
-            
-        }
+        for(int i=0;i<si;i++){
+             int res=mul*v[i];
+             res = res+carry;
+             v[i]=res%10;
+             carry=res/10;
+        } 
         while(carry>0){
-            arr[size]=carry%10;
-            size++;
+            v[si]= carry%10;
+            si++;
             carry = carry/10;
         }
-        
     }
-    
+
+
     vector<int> factorial(int n){
         // code here
-        vector<int>arr(10000,0);
-        arr[0]=1;
-        int size =1;
+        vector<int>v(10000,0);
+       int  si=1;
+        v[0]=1;
         for(int i=2;i<=n;i++){
-            multiply(arr,size,i);
+           multiply(v,si,i);
         }
-        vector<int>result;
-        for(int i=size-1;i>=0;i--){
-            result.push_back(arr[i]);
+        vector<int>res;
+        for(int i=si-1;i>=0;i--){
+            res.push_back(v[i]);
         }
-        return result ;
+        return res;
     }
 };
+
+
+//User function template for C++
+
+// class Solution {
+// public:
+
+//     void  multiply(vector<int>&arr,int &size,int mul){
+//         int carry =0;
+//         for(int i=0;i<size;i++){
+//             int res= mul*arr[i];
+//             res = res+carry;
+//             arr[i]=res%10;
+//             carry = res/10;
+            
+//         }
+//         while(carry>0){
+//             arr[size]=carry%10;
+//             size++;
+//             carry = carry/10;
+//         }
+        
+//     }
+    
+//     vector<int> factorial(int n){
+//         // code here
+//         vector<int>arr(10000,0);
+//         arr[0]=1;
+//         int size =1;
+//         for(int i=1;i<=n;i++){
+//             multiply(arr,size,i);
+//         }
+//         vector<int>result;
+//         for(int i=size-1;i>=0;i--){
+//             result.push_back(arr[i]);
+//         }
+//         return result ;
+//     }
+// };
+
+
+
 
 
 
