380c04209e500e67616907e036ac9f1482da1ee5
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-12-23 09:39:39 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find all factorial numbers less than or equal to N/find-all-factorial-numbers-less-than-or-equal-to-n.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+public:
+    vector<long long> factorialNumbers(long long N)
+    {
+ vector<long long> result;
+    
+    long long fact = 1;
+    long long i = 1;
+    
+    while (fact <= N) {
+        result.push_back(fact);
+        i++;
+        fact *= i;
+    }
+    
+    return result;
+        
+        
+        
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+        cin>>N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for(auto num : ans){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
