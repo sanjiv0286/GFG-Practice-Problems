a5ad517fe86054a3a1b1d4259ab946d054e3c7a8
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2023-12-25 13:19:59 +0530

    Updated Solution - GfG to GitHub

--- Easy/Series GP/series-gp.cpp
+++ Easy/Series GP/series-gp.cpp
@@ -4,46 +4,63 @@ using namespace std;
 
 // } Driver Code Ends
 
-// int m = 1e9+7;
+int m = 1e9+7;
 
-// class Solution{
-// 	public:
+class Solution{
+	public:
+	 
+	 long long power(long long x,long long n)
+    {
+       //Your code here
+       long long res=1;
+     
+       while(n>0){
+        // if(n%2!=0){
+      if(n&1){
+           res =(res*x)%m;
+       }
+      
+    //   n/=2;
+      n= n>>1;
+       x = (x*x)%m;
+       }
+       return res;
+    } 
 	
-//   	int Nth_term(int a, int r, int n){
-//   	    // Code here
-//   	    int res = ((a)*(pow(r,n-1)));
-//   	    return res%m;
-   	    
-//   	}    
-// };
-
-const int m = 1e9 + 7;
-
-class Solution {
-public:
-    long long power(long long x, long long y) {
-        long long res = 1;
-        x = x % m;
-
-        while (y > 0) {
-            if (y & 1) {
-                res = (res * x) % m;
-            }
-
-            y = y >> 1;
-            x = (x * x) % m;
-        }
-
-        return res;
-    }
-
-    int Nth_term(int a, int r, int n) {
-        // Code here
-        long long res = (a * power(r, n - 1)) % m;
-        return static_cast<int>(res);
-    }
+  	int Nth_term(int a, int r, int n){
+  	   long long   res = (a*power(r,n-1))%m;
+  	    return int(res);
+  	}
 };
 
+
+
+// const int m = 1e9 + 7;
+
+// class Solution {
+// public:
+//     long long power(long long x, long long y) {
+//         long long res = 1;
+//         x = x % m;
+
+//         while (y > 0) {
+//             if (y & 1) {
+//                 res = (res * x) % m;
+//             }
+
+//             y = y >> 1;
+//             x = (x * x) % m;
+//         }
+
+//         return res;
+//     }
+
+//     int Nth_term(int a, int r, int n) {
+//         // Code here
+//         long long res = (a * power(r, n - 1)) % m;
+//         return static_cast<int>(res);
+//     }
+// };
 
 
 //{ Driver Code Starts.
