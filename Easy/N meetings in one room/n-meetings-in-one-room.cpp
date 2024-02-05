52aad427aab1ed95685a676e78ea6980d4e453bd
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-02-05 11:35:35 +0530

    Updated Solution - GfG to GitHub

--- Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -4,36 +4,75 @@ using namespace std;
 
 // } Driver Code Ends
 
+// class Solution
+// {
+//     public:
+//     //Function to find the maximum number of meetings that can
+//     //be performed in a meeting room.
+// //   static bool cmp(pair<int,int>&a,pair<int,int>&b){
+// //         return a.second<b.second;
+// //     }
+// static bool cmp(pair<int,int>&a,pair<int,int>&b){
+//         return a.second<b.second;
+//     }
+    
+//     int maxMeetings(int s[], int e[], int n)
+//     {
+//         // Your code here
+//         // vector<pair<int,int>>payr(n);
+//          vector<pair<int,int>>payr;
+//         for(int i=0;i<n;i++){
+//         //   payr[i]= make_pair(s[i],e[i]);
+//         payr.push_back(make_pair(s[i],e[i]));
+//         }
+//         sort(payr.begin(),payr.end(),cmp);
+//         int end = payr[0].second;
+//         int ct=1;
+//         for(int i=1;i<n;i++){
+//             if(payr[i].first > end){
+//                 ct++;
+//                 end= payr[i].second;
+//             }
+//         }
+//         return ct;
+        
+//     }
+// };
 class Solution
 {
     public:
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-   static bool cmp(pair<int,int>&a,pair<int,int>&b){
+//   static bool cmp(pair<int,int>&a,pair<int,int>&b){
+//         return a.second<b.second;
+//     }
+static bool cmp(pair<int,int>&a,pair<int,int>&b){
         return a.second<b.second;
     }
     
     int maxMeetings(int s[], int e[], int n)
     {
         // Your code here
-        vector<pair<int,int>>payr(n);
+        // vector<pair<int,int>>payr(n);
+         vector<pair<int,int>>payr;
         for(int i=0;i<n;i++){
-           payr[i]= make_pair(s[i],e[i]);
+        //   payr[i]= make_pair(s[i],e[i]);
+        payr.push_back(make_pair(e[i],s[i]));
         }
-        sort(payr.begin(),payr.end(),cmp);
-        int end = payr[0].second;
+        // sort(payr.begin(),payr.end(),cmp);
+        sort(payr.begin(),payr.end());
+        int end = payr[0].first;
         int ct=1;
         for(int i=1;i<n;i++){
-            if(payr[i].first > end){
+            if(payr[i].second > end){
                 ct++;
-                end= payr[i].second;
+                end= payr[i].first;
             }
         }
         return ct;
         
     }
 };
-
 
 
 //{ Driver Code Starts.
