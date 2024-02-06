68262d00228d1cbab12b481969226cb46a67147b
Sanjiv Kushwaha <102908980+sanjiv0286@users.noreply.github.com>
2024-02-06 10:21:48 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reverse words in a given string/reverse-words-in-a-given-string.cpp
+++ Easy/Reverse words in a given string/reverse-words-in-a-given-string.cpp
@@ -12,20 +12,62 @@ class Solution
     //Function to reverse words in a given string.
     string reverseWords(string S) 
     { 
-        reverse(S.begin(),S.end());
-        string res=""; 
-        int start =0;
-        for(int i=0;i<S.size();i++){
+        // reverse(S.begin(),S.end());
+        // string res=""; 
+        // int start =0;
+        // for(int i=0;i<S.size();i++){
+        //     if(S[i]=='.'){
+        //         reverse(S.begin()+start,S.begin()+i);
+        //          res += S.substr(start, i - start)+".";
+        //         start = i+1;
+        //     }
+        // }
+        // reverse(S.begin()+start,S.end());
+        // res += S.substr(start,S.size()-start);
+        // return res;
+        // ******************************************
+        // string res="";
+        // string temp="";
+        // for(int i=S.size()-1;i>=0;i--){
+        //     if(S[i]=='.'){
+        //          reverse(temp.begin(),temp.end());
+        //          res = res +temp+".";
+        //          temp ="";
+        //     }
+        //     else{
+        //         temp.push_back(S[i]);
+        //     }
+        // }
+        // reverse(temp.begin(),temp.end());
+        // res = res + temp;
+        // return res;
+        // **************************************************
+        string temp ="";
+        string ans = "";
+        stack<string>st;
+        int n = S.size();
+        for(int i=0;i<n;i++){
             if(S[i]=='.'){
-                reverse(S.begin()+start,S.begin()+i);
-                 res += S.substr(start, i - start)+".";
-                start = i+1;
+                st.push(temp);
+                temp ="";
+            }
+            else{
+                temp+=S[i];
             }
         }
-        reverse(S.begin()+start,S.end());
-        res += S.substr(start,S.size()-start);
-        return res;
-    } 
+        st.push(temp);
+        
+        while(st.size()>0){
+            if(st.size()>1){
+                ans = ans + st.top() + ".";
+            }
+            else{
+                ans = ans + st.top();
+            }
+            st.pop();
+        }
+        return ans;
+    }
 };
 
 
